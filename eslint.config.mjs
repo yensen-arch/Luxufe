79da51e489c3743dac02cf92c171f7863a71de20
Yensen Arch <officialdevain25@gmail.com>
2025-06-21 18:01:46 +0530

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@next/next/no-img-element": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
