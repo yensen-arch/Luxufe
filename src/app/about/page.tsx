cd6b7a2ec4f6450964eb69a3651042907bb73856
Yensen Arch <officialdevain25@gmail.com>
2025-07-03 17:13:32 +0530

    .

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import AboutHero from "@/components/aboutUs/AboutHero";
 import Navbar from "@/components/common/Navbar";
+import LuxuryPartners from "@/components/landing/LuxuryPartners";
+import ElevePerks from "@/components/landing/ElevePerks";
+import ContactUs from "@/components/landing/ContactUs";
+import Footer from "@/components/common/Footer";
 
 export default function About() {
   return (
     <main>
       <Navbar />
       <AboutHero />
+      <LuxuryPartners />
+      <ElevePerks />
+      <ContactUs />
+      <Footer />
     </main>
   );
 }
