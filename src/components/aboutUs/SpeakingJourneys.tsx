1b1102d619524ca5936130638fdc31b1fc432934
Yensen Arch <officialdevain25@gmail.com>
2025-07-03 21:27:34 +0530

    .

--- src/components/aboutUs/SpeakingJourneys.tsx
+++ src/components/aboutUs/SpeakingJourneys.tsx
@@ -36,16 +36,28 @@ export default function SpeakingJourneys() {
         <div className="grid grid-cols-12 h-full items-center relative">
           {/* Left Content Section */}
           <div className="bg-[url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=600&q=80')] bg-cover bg-center lg:col-span-12 h-full text-center lg:text-left p-6 lg:p-24 flex items-center">
-          <div className="w-3/7">
+            <div className="w-3/7 flex flex-col items-start justify-center">
               <h2 className="text-5xl font-arpona font-bold text-white my-6">
-                Inspiration for the discerning traveller
+                Journeys that speak for themselves
               </h2>
-              <p className="mb-10 lg:w-5/6 font-inter font-bold text-white">
-                Explore expert insights, curated guides, and insider tips to elevate your travel experiences. 
-                Discover the world's finest destinations, luxury stays, and bespoke journeys, all designed to 
-                inspire your next adventure.
+              <p className="mb-10 lg:w-5/7 font-inter font-bold text-white">
+                Luxufe is a travel company that specializes in creating bespoke journeys for discerning travelers.
               </p>
-              
+              {/* Navigation Buttons */}
+              <div className="flex gap-6 mt-8 mx-auto lg:mx-0">
+                <button 
+                  onClick={scrollPrev} 
+                  className="bg-white rounded-full p-6 shadow-md hover:bg-white transition"
+                >
+                  <ArrowLeft className="h-6 w-6 text-gray-800" />
+                </button>
+                <button 
+                  onClick={scrollNext} 
+                  className="bg-white rounded-full p-6 shadow-md hover:bg-white transition"
+                >
+                  <ArrowRight className="h-6 w-6 text-gray-800" />
+                </button>
+              </div>
             </div>
           </div>
 
@@ -62,22 +74,6 @@ export default function SpeakingJourneys() {
                   </div>
                 ))}
               </div>
-            </div>
-            
-            {/* Navigation Buttons */}
-            <div className="absolute flex flex-col gap-6 top-1/2 left-3/4 -translate-x-1/2 -translate-y-1/2  pointer-events-none z-10">
-              <button 
-                onClick={scrollPrev} 
-                className="bg-white rounded-full p-6 shadow-md hover:bg-white transition pointer-events-auto "
-              >
-                <ArrowLeft className="h-6 w-6 text-gray-800" />
-              </button>
-              <button 
-                onClick={scrollNext} 
-                className="bg-white rounded-full p-6 shadow-md hover:bg-white transition pointer-events-auto"
-              >
-                <ArrowRight className="h-6 w-6 text-gray-800" />
-              </button>
             </div>
           </div>
         </div>
