f379e8108daa1f2e28736e1fc08a9ab3e4edfa93
Yensen Arch <officialdevain25@gmail.com>
2025-07-05 10:42:20 +0530

    .

--- src/components/blogs/EffortlessLuxury.tsx
+++ src/components/blogs/EffortlessLuxury.tsx
@@ -9,17 +9,14 @@ export default function EffortlessLuxury() {
           
           {/* Right: Text */}
           <div className="md:w-1/2 w-full flex flex-col justify-center my-auto ml-10">
-            <h3 className="text-5xl md:text-3xl font-arpona font-bold text-[#23263a] mb-8">
-              The Art of Effortless Travel
+            <h3 className="text-5xl md:text-5xl font-arpona font-bold text-[#23263a] mb-8 leading-tight">
+              The Art of <br /> Effortless Travel
             </h3>
-            <p className="font-inter text-[#23263a] md:text-md mb-4 w-6/8 font-bold">
-              We believe that true luxury is felt, not just seen. It is the ease of knowing every detail has been anticipated, the quiet confidence of seamless transitions, and the joy of experiencing something truly personal. Travel should never feel complicated or transactional, but rather it should unfold naturally.
-            </p>
             <p className="font-inter text-[#23263a] md:text-md mb-8 w-6/8 font-bold">
-              From the first touchpoint to the final farewell, our ethos ensures that luxury is not just what you experience, but how effortlessly you experience it
+              We believe that true luxury lies in the details, where every moment is seamlessly orchestrated and every journey is as effortless as it is extraordinary.
             </p>
-            <button className="border text-xs border-slate-300 px-8 py-4 font-inter font-semibold text-[#23263a] flex items-center gap-2 w-fit hover:bg-black hover:text-white transition-colors">
-              EXPLORE LUXURY
+            <button className="border border-slate-400 px-8 py-4 font-inter font-semibold text-[#23263a] text-xs flex items-center gap-2 w-fit hover:bg-black hover:text-white transition-colors">
+              MORE ABOUT US
               <span className="ml-2">&rarr;</span>
             </button>
           </div>
