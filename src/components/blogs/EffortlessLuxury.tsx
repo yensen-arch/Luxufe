020485bdebf78b54441698b762676a2071387131
Yensen Arch <officialdevain25@gmail.com>
2025-07-05 10:46:26 +0530

    .

--- src/components/blogs/EffortlessLuxury.tsx
+++ src/components/blogs/EffortlessLuxury.tsx
@@ -5,10 +5,9 @@ export default function EffortlessLuxury() {
     <section className="py-24 bg-white">
       <div className="max-w-7xl mx-auto px-4">
         {/* Content Row */}
-        <div className="flex flex-col md:flex-row items-start gap-12">
-          
-          {/* Right: Text */}
-          <div className="md:w-1/2 w-full flex flex-col justify-center my-auto ml-10">
+        <div className="flex flex-col md:flex-row">
+          {/* Left: Text */}
+          <div className="md:w-1/2 w-full flex flex-col justify-center my-auto ml-20">
             <h3 className="text-5xl md:text-5xl font-arpona font-bold text-[#23263a] mb-8 leading-tight">
               The Art of <br /> Effortless Travel
             </h3>
@@ -20,12 +19,12 @@ export default function EffortlessLuxury() {
               <span className="ml-2">&rarr;</span>
             </button>
           </div>
-          {/* Left: Image */}
+          {/* Right: Image */}
           <div className="md:w-1/2 flex-shrink-0">
             <img
               src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80"
               alt="Santorini"
-              className="w-[450px] h-[340px] md:h-[450px] mx-auto object-cover "
+              className="w-[450px] h-[340px] md:h-[450px] mx-auto object-cover shadow-lg"
             />
           </div>
         </div>
