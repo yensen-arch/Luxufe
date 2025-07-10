a63bfc8e1f1c0b1c3516b9da0fe3314fa891595f
Yensen Arch <officialdevain25@gmail.com>
2025-07-10 13:27:31 +0530

    .

--- src/components/contact-us/Carousel.tsx
+++ src/components/contact-us/Carousel.tsx
@@ -1,24 +1,20 @@
 export default function Carousel() {
-
   return (
-    <section className="overflow-hidden mt-20 h-screen">
-      <div className="container mx-auto">
-        <div className="relative flex flex-col lg:flex-row items-center lg:justify-end min-h-[650px]">
-          {/* Base Section */}
-          <div className="bg-gray-100 w-8/8 mt-8 h-[80vh] lg:absolute">
-          </div>
-          {/* Image Section */}
-          <div className="w-5/7 h-full z-10 -translate-x-1/2 -top-40 relative left-1/2">
-            <div className=" overflow-hidden shadow-2xl">
-              <img
-                src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=600&q=80"
-                alt="Cocktail"
-                className="w-full h-full object-cover"
-              />
-            </div>
-          </div>
-          
-        </div>
+    <section className="relative w-full h-[80vh] flex items-center justify-center bg-transparent my-70">
+      {/* Background Sheet */}
+      <div
+        className="absolute left-1/2 top-1/2 w-full h-full bg-gray-100 -translate-x-1/2 z-0"
+        aria-hidden="true"
+      />
+      {/* Foreground Card (Image) */}
+      <div
+        className="absolute left-1/2 top-1/2 w-[80vw] h-full bg-white shadow-[0_10px_40px_rgba(0,0,0,0.15)] flex items-center justify-center -translate-x-1/2 -translate-y-1/2 z-10"
+      >
+        <img
+          src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=800&q=80"
+          alt="Cocktail"
+          className="w-full h-full object-cover"
+        />
       </div>
     </section>
   )
