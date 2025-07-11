a12b8c94f168c4a16437661364bba79d8f7d2255
Yensen Arch <officialdevain25@gmail.com>
2025-07-11 11:57:46 +0530

    .

--- src/components/waysToTravel/WaysToTravelIntro.tsx
+++ src/components/waysToTravel/WaysToTravelIntro.tsx
@@ -11,7 +11,7 @@ export default function WaysToTravelIntro() {
             <span className="text-gray-400 text-3xl font-bold">Logo</span>
           </div>
         </div>
-        <p className="text-2xl md:text-3xl font-arpona text-[#23263a] font-bold text-center max-w-3xl">
+        <p className="text-2xl md:text-3xl font-arpona text-[#23263a] font-bold text-center w-1/2">
         Whether you’re drawn to the wild, the serene, the cultural, or the indulgent, Luxufe curates experiences around what matters most to you. Start with what you love, and we’ll build the journey around it.
         </p>
       </div>
