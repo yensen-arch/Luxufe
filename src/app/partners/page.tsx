61953b445cfc3531ae551d5e571153c804019b18
Yensen Arch <officialdevain25@gmail.com>
2025-07-13 11:58:01 +0530

    .

--- src/app/partners/page.tsx
+++ src/app/partners/page.tsx
@@ -5,12 +5,14 @@ import HeroPartners from "@/components/partners/HeroPartners";
 import ElevePerks from "@/components/landing/ElevePerks";
 import SelectedFavs from "@/components/partners/SelectedFavs";
 import PartnerHighlight from "@/components/partners/PartnerHighlight";
+import BenefitsSection from "@/components/partners/BenefitsSection";
 
 const Page = () => {
   return (
     <main>
       <Navbar />
       <HeroPartners />
+      <BenefitsSection />
       <PartnerHighlight />
       <SelectedFavs />
       <ElevePerks />
