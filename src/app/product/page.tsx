fd456ec1f696e0dd510e9e20fdd828787aa72115
Yensen Arch <officialdevain25@gmail.com>
2025-07-14 15:46:18 +0530

    .

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -3,17 +3,18 @@ import Footer from "@/components/common/Footer";
 import ContactUs from "@/components/regions/ContactUs";
 import Testimonials from "@/components/landing/Testimonials";
 import ProductHero from "@/components/Product/ProductHero";
-import WhyWeTravel from "@/components/brand/WhyWeTravel";
-import BrandBenefits from "@/components/brand/BrandBenefits";
 import Itineraries from "@/components/brand/Itineraries";
-import BrandMain from "@/components/brand/BrandMain";
 import ProductOverview from "@/components/Product/ProductOverview";
+import ProductGrid from "@/components/Product/Grid";
+import MapProduct from "@/components/Product/MapProduct";
 const Page = () => {
   return (
     <main className="overflow-y-hidden">
       <Navbar />
       <ProductHero />
       <ProductOverview />
+      <ProductGrid />
+      <MapProduct />
       <Itineraries />
       <Testimonials />
       <ContactUs />
