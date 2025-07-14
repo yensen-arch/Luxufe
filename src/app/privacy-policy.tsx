89bf62cf194b3646965a337882a9fbec7a0c0a3a
Yensen Arch <officialdevain25@gmail.com>
2025-07-14 16:08:29 +0530

    .

---
+++ src/app/privacy-policy.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
+import ExploreMore from "@/components/eleve/ExploreMore";
+
+export default function Page() {
+  return (
+    <main>
+      <Navbar />
+      <ExploreMore />
+      <Footer />
+    </main>
+  );
+};
