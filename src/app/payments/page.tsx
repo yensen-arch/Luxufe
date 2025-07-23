23fedc1289dc3affb1844ca6ef63c759531c6d91
Yensen Arch <officialdevain25@gmail.com>
2025-07-23 15:21:43 +0530

    done

---
+++ src/app/payments/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
+import ContactUs from "@/components/regions/ContactUs";
+
+const PaymentsPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <ContactUs />
+        <Footer />
+    </div>
+  )
+}
+
+export default PaymentsPage;
