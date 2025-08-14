bf9dcf7825ba51b6fccb5770ec1b120d2ed7f824
Yensen Arch <officialdevain25@gmail.com>
2025-08-14 17:42:46 +0530

    .

--- src/components/itineraries/land-itineraries/PricingAndHotels.tsx
+++ src/components/itineraries/land-itineraries/PricingAndHotels.tsx
@@ -6,12 +6,12 @@ import GoodToKnowAccordion from "./GoodToKnowAccordion";
 
 export default function PricingAndHotelsSection() {
   return (
-    <section className="bg-white border-t border-l border-gray-300">
+    <section className="bg-white border-t-2 border-gray-300">
       <div className="mx-auto flex flex-col md:flex-row">
-        <div className="md:w-1/4 w-full md:pr-8 p-6 md:p-12 border-r border-gray-300">
+        <div className="md:w-1/4 w-full md:pr-8 p-6 md:p-12 border-r-2 border-gray-300">
           <PricingOptions />
         </div>
-        <div className="md:w-3/4 w-full p-6 md:p-12 border-t border-l border-gray-300">
+        <div className="md:w-3/4 w-full p-6 md:p-12">
           <AvailableHotels />
           <VisitView />
           <TripExtensions />
