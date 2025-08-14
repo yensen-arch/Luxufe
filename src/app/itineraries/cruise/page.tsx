3ed7c8ddb661c3cefddcacde7e580a8c96fb1b7b
Yensen Arch <officialdevain25@gmail.com>
2025-08-14 16:33:35 +0530

    .

--- src/app/itineraries/cruise/page.tsx
+++ src/app/itineraries/cruise/page.tsx
@@ -1,11 +1,11 @@
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
-import Hero from "@/components/journeys/Hero";
-import ItineraryDetails from "@/components/journeys/ItineraryDetails";
-import PricingAndCabinsSection from "@/components/journeys/PricingAndCabinsSection";
+import Hero from "@/components/itineraries/cruise-itineraries/Hero";
+import ItineraryDetails from "@/components/itineraries/cruise-itineraries/ItineraryDetails";
+import PricingAndCabinsSection from "@/components/itineraries/cruise-itineraries/PricingAndCabinsSection";
 import ContactUsJourneys from "@/components/landing/ContactUsJourneys";
 
-export default function Journeys() {
+export default function CruiseItineraries() {
   return (
     <main>
       <Navbar />
