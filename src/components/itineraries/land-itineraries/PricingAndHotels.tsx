723905ca090199296c3ba7e6f4f603f50449511c
Yensen Arch <officialdevain25@gmail.com>
2025-08-15 08:57:12 +0530

    .

--- src/components/itineraries/land-itineraries/PricingAndHotels.tsx
+++ src/components/itineraries/land-itineraries/PricingAndHotels.tsx
@@ -1,13 +1,17 @@
 import VisitView from "./VisitView";
 import GoodToKnowAccordion from "./GoodToKnowAccordion";
+import PricingOptions from "./PricingOptions";
+import Accommodation from "./Accommodation";
 
 export default function PricingAndHotelsSection() {
   return (
-    <section className="bg-white border-t-2 border-gray-300">
+    <section className="bg-white border-y-2 border-gray-300">
       <div className="mx-auto flex flex-col md:flex-row">
         <div className="md:w-1/4 w-full md:pr-8 p-6 md:p-12 border-r-2 border-gray-300">
         </div>
         <div className="md:w-3/4 w-full p-6 md:p-12">
+          <PricingOptions />
+          <Accommodation />
           <GoodToKnowAccordion />
           <VisitView />
         </div>
