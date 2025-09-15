d1b962fcf5897fffdd54a547056b769abca00292
Yensen Arch <officialdevain25@gmail.com>
2025-09-15 11:38:43 +0530

    .

--- src/app/itineraries/land/page.tsx
+++ src/app/itineraries/land/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
-import LandItinerariesHero from "@/components/land-itineraries/LandItinerariesHero";
-import LandItinerariesMain from "@/components/land-itineraries/LandItinerariesMain";
+import LandItinerariesHero from "@/components/itineraries/land-itineraries/LandItinerariesHero";
+import LandItinerariesMain from "@/components/itineraries/land-itineraries/LandItinerariesMain";
 import { getLandItinerariesPageData } from "@/lib/sanity/landItinerariesPage";
 import ContactUs from "@/components/partners/ContactUs";
 import Testimonials from "@/components/landing/Testimonials";
