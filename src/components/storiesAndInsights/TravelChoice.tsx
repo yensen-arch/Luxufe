482441547b5ac232357770cbab270bde64fda74b
Yensen Arch <officialdevain25@gmail.com>
2025-09-27 15:35:00 +0530

    .

--- src/components/storiesAndInsights/TravelChoice.tsx
+++ src/components/storiesAndInsights/TravelChoice.tsx
@@ -39,35 +39,13 @@ export default function TravelChoice({ data }: TravelChoiceProps) {
   const getSocialIcon = (platform: string) => {
     switch (platform.toLowerCase()) {
       case 'instagram':
-        return <Image src="/instagram.svg" alt="Instagram" width={24} height={24} />
+        return <Image src="/Icon-simple-instagram-black.svg" alt="Instagram" width={24} height={24} />
       case 'facebook':
-        return <Image src="/facebook.svg" alt="Facebook" width={24} height={24} />
+        return <Image src="/Icon-awesome-facebook-black.svg" alt="Facebook" width={24} height={24} />
       case 'tripadvisor':
-        return (
-          <span aria-label="TripAdvisor">
-            <svg className="h-5 w-5 md:h-6 md:w-6 lg:h-7 lg:w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="7.5" cy="12" r="3.5" stroke="#23263a" strokeWidth="1.5" fill="white" />
-              <circle cx="16.5" cy="12" r="3.5" stroke="#23263a" strokeWidth="1.5" fill="white" />
-              <circle cx="7.5" cy="12" r="1.5" fill="#23263a" />
-              <circle cx="16.5" cy="12" r="1.5" fill="#23263a" />
-              <path d="M2 12c2-2 6-2 8 0" stroke="#23263a" strokeWidth="1.5" fill="none" />
-              <path d="M22 12c-2-2-6-2-8 0" stroke="#23263a" strokeWidth="1.5" fill="none" />
-            </svg>
-          </span>
-        )
+        return <Image src="/Icon-awesome-tripadvisor-black.svg" alt="TripAdvisor" width={24} height={24} />
       case 'google':
-        return (
-          <span aria-label="Google">
-            <svg className="h-5 w-5 md:h-6 md:w-6 lg:h-7 lg:w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g>
-                <path d="M21.805 12.082c0-.638-.057-1.252-.163-1.837H12.22v3.48h5.43a4.637 4.637 0 0 1-2.01 3.045v2.522h3.25c1.9-1.75 2.915-4.33 2.915-7.21z" fill="#4285F4"/>
-                <path d="M12.22 22c2.43 0 4.47-.805 5.96-2.18l-3.25-2.522c-.9.6-2.05.96-3.32.96-2.55 0-4.71-1.72-5.48-4.03H3.77v2.57A9.78 9.78 0 0 0 12.22 22z" fill="#34A853"/>
-                <path d="M6.74 14.228a5.89 5.89 0 0 1 0-3.756v-2.57H3.77a9.78 9.78 0 0 0 0 8.896l2.97-2.57z" fill="#FBBC05"/>
-                <path d="M12.22 7.54c1.32 0 2.5.454 3.43 1.345l2.57-2.57C16.69 4.805 14.65 4 12.22 4A9.78 9.78 0 0 0 3.77 7.902l2.97 2.57c.77-2.31 2.93-4.03 5.48-4.03z" fill="#EA4335"/>
-              </g>
-            </svg>
-          </span>
-        )
+        return <Image src="/Icon-ionic-logo-google-black.svg" alt="Google" width={24} height={24} />        
       default:
         return null
     }
