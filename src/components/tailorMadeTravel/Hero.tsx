35613d782f5a5782a551f69495097c49da4af36f
Yensen Arch <officialdevain25@gmail.com>
2025-09-28 13:24:07 +0530

    .

--- src/components/tailorMadeTravel/Hero.tsx
+++ src/components/tailorMadeTravel/Hero.tsx
@@ -29,12 +29,12 @@ export default function Hero({ data }: HeroProps) {
       >
         <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
       </div>
-      <div className="relative z-10 p-4 md:p-6 lg:p-8 flex flex-col items-center justify-center w-full">
-        <p className="text-xl md:text-2xl lg:text-4xl xl:text-5xl mb-4 md:mb-5 lg:mb-6 font-bellarina px-4" style={{fontFamily: 'Bellarina, serif'}}>{displaySubtitle}</p>
-        <h1 className="w-2xl text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-arpona font-light mb-4 md:mb-5 lg:mb-6 leading-tight px-4">
+      <div className="relative z-10 p-3 sm:p-4 md:p-6 lg:p-8 flex flex-col items-center justify-center w-full">
+        <p className="text-xl sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl mb-3 sm:mb-4 md:mb-5 lg:mb-6 font-bellarina px-2 sm:px-4" style={{fontFamily: 'Bellarina, serif'}}>{displaySubtitle}</p>
+        <h1 className="w-2xl text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-arpona font-light mb-3 sm:mb-4 md:mb-5 lg:mb-6 leading-tight px-2 sm:px-4">
           {displayTitle}
         </h1>
-        <p className="mt-2 max-w-xl mx-auto font-inter text-sm md:text-base lg:text-lg font-bold px-4">
+        <p className="mt-2 max-w-xs sm:max-w-sm md:max-w-xl mx-auto font-inter text-xs sm:text-sm md:text-base lg:text-lg font-bold px-2 sm:px-4">
           {displayDescription}
         </p>
       </div>
