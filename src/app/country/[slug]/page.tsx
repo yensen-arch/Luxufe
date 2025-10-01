5b5e4542bd0881e525e05d03fad4110d7a356e3d
Yensen Arch <officialdevain25@gmail.com>
2025-10-01 10:33:23 +0530

    .

--- src/app/country/[slug]/page.tsx
+++ src/app/country/[slug]/page.tsx
@@ -38,7 +38,7 @@ const Page = async ({ params }: PageProps) => {
   }
 
   return (
-    <main>
+    <main className="overflow-x-hidden">
       <Navbar />
       <CountryHero 
         countryName={countryName}
--- src/components/country/GoodToKnow.tsx
+++ src/components/country/GoodToKnow.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-
+import Image from "next/image";
 const items = [
   {
     title: "Visas, Entry Requirements & Exclusive Travel Tips",
@@ -41,17 +41,19 @@ export default function GoodToKnow() {
     <section className="py-24 bg-[#f5f6f7]">
       <div className="max-w-7xl mx-auto px-4">
         {/* Heading */}
-        <h2 className="text-4xl md:text-5xl w-8xl font-arpona font-bold text-[#23263a] mb-16">
-          Essential Travel Information<br />for Luxury Voyagers
+        <h2 className="text-4xl md:text-5xl w-3/5 font-arpona font-bold text-[#23263a] mb-16">
+          Essential Travel Information for Luxury Voyagers
         </h2>
         {/* Content Row */}
         <div className="flex flex-col md:flex-row items-start gap-12">
           {/* Left: Image */}
           <div className="md:w-1/2 w-full flex-shrink-0">
-            <img
+            <Image
               src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80"
               alt="Santorini"
-              className="w-full h-[340px] md:h-[420px] object-cover "
+              className="w-full h-[340px] md:h-[460px] object-cover "
+              width={800}
+              height={600}
             />
           </div>
           {/* Right: Text */}
@@ -87,31 +89,8 @@ export default function GoodToKnow() {
               {/* Icon */}
               <span className="ml-4 text-gray-500 group-hover:text-gray-800">
                 {openIndex === idx ? (
-                  <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 32 32"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <line x1="8" y1="8" x2="24" y2="24" />
-                    <line x1="24" y1="8" x2="8" y2="24" />
-                  </svg>
-                ) : (
-                  <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 32 32"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <line x1="16" y1="8" x2="16" y2="24" />
-                    <line x1="8" y1="16" x2="24" y2="16" />
-                  </svg>
-                )}
-              </span>
+                 
+              </span> */}
             </button>
 
             {/* Expandable panel */}
