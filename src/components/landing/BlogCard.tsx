46faad8721803482a215d92fd766745d67ce4bb9
Yensen Arch <officialdevain25@gmail.com>
2025-10-04 19:01:01 +0530

    .

--- src/components/landing/BlogCard.tsx
+++ src/components/landing/BlogCard.tsx
@@ -26,7 +26,7 @@ const BlogCard: React.FC<BlogCardProps> = ({
   const fallbackButtonText = buttonText || "READ MORE";
   const fallbackButtonLink = buttonLink || "#";
   return (
-    <div className="w-110 max-h-170   bg-blue-500 relative border border-gray-200 shadow-xl overflow-hidden">
+    <div className="w-110 h-170 bg-blue-500 relative border border-gray-200 shadow-xl overflow-hidden">
       {/* Image will occupy the full top space */}
       <img
         src={fallbackImageUrl}
@@ -40,14 +40,14 @@ const BlogCard: React.FC<BlogCardProps> = ({
       >
         <Image src="/custom_curve.svg" alt="Curve" width={200} height={700} className="w-full h-full object-cover" />
       </div>
-      <div className="relative px-4 sm:px-6 lg:px-5 py-0 text-left" style={{ zIndex: 20, marginTop: "-380px" }}>
+      <div className="relative px-4 sm:px-6 lg:px-5 py-0 text-left" style={{ zIndex: 20, marginTop: "-280px" }}>
         <p className="text-xs text-gray-500 tracking-widest uppercase font-inter font-bold">
           {fallbackCategory}
         </p>
-        <h3 className="text-lg sm:text-xl font-arpona font-bold text-gray-800 mt-2 mb-6 h-auto sm:h-16 w-full sm:w-2/4">
+        <h3 className="text-2xl sm:text-md font-arpona font-bold text-gray-800 mt-4 mb-6 h-auto sm:h-16 w-full sm:w-2/4">
           {fallbackTitle}
         </h3>
-        <p className="text-[10px] sm:text-xs text-gray-500 w-full sm:w-3/4 leading-relaxed h-auto sm:h-24 font-inter font-bold mb-1 sm:mb-1">
+        <p className="text-xs text-gray-500 w-2/3 leading-relaxed h-auto sm:h-24 font-inter font-bold mb-1 sm:mb-1">
           {fallbackDescription}
         </p>
         <a
